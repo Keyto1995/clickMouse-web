6c84a4e7bc8fcbe49b17192bfce9d789f77ecf59
Keyto <keyto1995@outlook.com>
2025-04-04 08:53:16 +0800

    refactor(src): 重构 App 组件并添加新功能
    - 移除 index.html 中的 favicon 链接和 vite.svg 文件
    - 重构 App 组件，使用 map 函数动态生成菜单项
    - 添加 handleClick 函数统一处理点击事件
    - 优化按钮点击逻辑，实现异步请求

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,53 @@
 import React from "react";
 import { Button, Collapse } from "antd-mobile";
 
+const menuItems = [{
+  action: "a",
+  title: "与武器商人交谈"
+}, {
+  action: "b",
+  title: "与盔甲商人交谈"
+}, {
+  action: "c",
+  title: "与马匹商人交谈"
+}, {
+  action: "d",
+  title: "与杂货商人交谈"
+}];
+
+const handleClick = async (action: string, times: number) => {
+  const res = await fetch(`/api/${action}?times=${times}`);
+  if (res.ok) {
+    const data = await res.json();
+    console.log(data);
+  } else {
+    throw new Error("请求失败");
+  }
+}
+
 const App: React.FC = () => {
 
   return (<>
     <h1>骑马与砍杀：战团</h1>
 
     <Collapse accordion>
-      <Collapse.Panel key="1" title="与武器商人交谈">
-        <Button onClick={() => {
-          fetch("/api/ping").then(res => res.json()).then(data => {
-            console.log(data);
-          }).catch(err => {
-            console.error(err);
-          })
-            ;
-
-        }}>x10</Button>
-        <Button>x50</Button>
-        <Button>x100</Button>
-      </Collapse.Panel>
-      <Collapse.Panel key="2" title="与盔甲商人交谈">
-        <Button>x10</Button><Button>x50</Button><Button>x100</Button>
-      </Collapse.Panel>
-      <Collapse.Panel key="3" title="与马匹商人交谈">
-        <Button>x10</Button><Button>x50</Button><Button>x100</Button>
-      </Collapse.Panel>
-      <Collapse.Panel key="4" title="与杂货商人交谈">
-        <Button>x10</Button><Button>x50</Button><Button>x100</Button>
-      </Collapse.Panel>
+      {menuItems.map((item) => {
+        return (
+          <Collapse.Panel key={item.action} title={item.title}>
+            <Button onClick={() => {
+              handleClick(item.action, 10);
+            }}>x10</Button>
+            <Button onClick={() => {
+              handleClick(item.action, 20);
+            }}>x20</Button>
+            <Button onClick={() => {
+              handleClick(item.action, 50);
+            }}>x50</Button>
+            <Button onClick={() => {
+              handleClick(item.action, 100);
+            }}>x100</Button>
+          </Collapse.Panel>)
+      })}
     </Collapse>
   </>)
 }
